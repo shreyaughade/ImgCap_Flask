d21243246655d2cc5c9c5c0efbc933f7870895e6
shreyaughade <shreyaughade03@gmail.com>
2022-02-24 15:24:58 +0530

    adding img captioning files

--- app.py
+++ app.py
@@ -1,84 +1,108 @@
-from __future__ import division, print_function
-# coding=utf-8
+from flask import Flask,render_template,url_for,request,jsonify,redirect
+from werkzeug.utils import secure_filename
+
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.xception import Xception
+from keras.models import load_model
+from pickle import load
+import numpy as np
+from PIL import Image
+
+import tempfile
 import sys
 import os
 import glob
 import re
-import numpy as np
 
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-
-# Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'model.h5'
 
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG"]
 
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
+@app.route('/')
+def home():
+	return render_template('index.html')
 
 
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+	def extract_features(filename, model):
+		try:
+			image = Image.open(filename)
+		except:
+			print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+		image = image.resize((299,299))
+		image = np.array(image)
+		if image.shape[2] == 4:
+			image = image[..., :3]
+		image = np.expand_dims(image, axis=0)
+		image = image/127.5
+		image = image - 1.0
+		feature = model.predict(image)
+		return feature
+	
+	def word_for_id(integer, tokenizer):
+		for word, index in tokenizer.word_index.items():
+			if index == integer:
+				return word
+		return None
 
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
+	def generate_desc(model, tokenizer, photo, max_length):
+		in_text = 'start'
+		for i in range(max_length):
+			sequence = tokenizer.texts_to_sequences([in_text])[0]
+			sequence = pad_sequences([sequence], maxlen=max_length)
+			pred = model.predict([photo,sequence], verbose=0)
+			pred = np.argmax(pred)
+			word = word_for_id(pred, tokenizer)
+			if word is None:
+				break
+			in_text += ' ' + word
+			if word == 'end':
+				break
+		return in_text
+	#path = 'Flicker8k_Dataset/111537222_07e56d5a30.jpg'
+	max_length = 32
+	tokenizer = load(open("tokenizer.p","rb"))
+	model = load_model('models/model_9.h5')
+	xception_model = Xception(include_top=False, pooling="avg")
 
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
+	def allowed_image(fname):
+		if not "." in fname:
+			return False
+		ext = fname.rsplit(".", 1)[1]
+		if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+			return True
+		else:
+			return False
+
+
+
+	if request.method == 'POST':
+	   
+	    f = request.files['file']
+	    fname=secure_filename(f.filename)
+
+	    if allowed_image(fname):
+	    	basepath = os.path.dirname(__file__)
+	    	file_path = os.path.join(basepath, 'uploads', fname)
+	    	f.save(file_path)
+	    	photo = extract_features(file_path, xception_model)
+	    	description = generate_desc(model, tokenizer, photo, max_length)
+	    	result= description[6:-3]
+	    	if os.path.exists(file_path):
+	    		os.remove(file_path)
+	    	return result
+	    else:
+	    	return 'Error occured, Please ensure you\'re using jpeg or jpg file format.' 
+	return None
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,54 +1,55 @@
 $(document).ready(function () {
-  // Init
-  $('.image-section').hide();
-  $('.loader').hide();
-  $('#result').hide();
+    // Init
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result').hide();
 
-  // Upload Preview
-  function readURL(input) {
-      if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-              $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-              $('#imagePreview').hide();
-              $('#imagePreview').fadeIn(650);
-          }
-          reader.readAsDataURL(input.files[0]);
-      }
-  }
-  $("#imageUpload").change(function () {
-      $('.image-section').show();
-      $('#btn-predict').show();
-      $('#result').text('');
-      $('#result').hide();
-      readURL(this);
-  });
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
 
-  // Predict
-  $('#btn-predict').click(function () {
-      var form_data = new FormData($('#upload-file')[0]);
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function () {
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+    });
 
-      // Show loading animation
-      $(this).hide();
-      $('.loader').show();
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
 
-      // Make prediction by calling api /predict
-      $.ajax({
-          type: 'POST',
-          url: '/predict',
-          data: form_data,
-          contentType: false,
-          cache: false,
-          processData: false,
-          async: true,
-          success: function (data) {
-              // Get and display the result
-              $('.loader').hide();
-              $('#result').fadeIn(600);
-              $('#result').text(' Result:  ' + data);
-              console.log('Success!');
-          },
-      });
-  });
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+                $('#result').fadeIn(600);
+                $('#result').text(' Generated Caption:  ' + data);
+                console.log('Success!');
+            },
+        });
+    });
 
 });
