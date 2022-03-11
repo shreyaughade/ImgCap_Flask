3d566348f5d1c299700830ea3dcc0ec48d2cfcaa
shreyaughade <shreyaughade03@gmail.com>
2022-03-11 17:30:52 +0530

    committing

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 from flask import Flask, render_template, request, jsonify
 from werkzeug.utils import secure_filename
+from PIL import Image
 import os
 
+port = int(os.environ.get('PORT', 5000))
 UPLOAD_FOLDER = os.getcwd() + "/images"
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
-  return jsonify({"Message":"Hello"})
+  return jsonify({"message": "Hello"})
 
 @app.route("/uploadImage", methods=["POST"])
 def upload():
-    if request.method == "POST":
-        print(request)
-        image = request.files["image"]
-        filename = secure_filename(image.filename)
-        print(filename)
-        image.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-        return jsonify({"message": "Image Uploaded Successfully"})
+  file = request.files['image']
+  img = Image.open(file.stream)
+  return jsonify({'message': 'success', 'size': [img.width, img.height]})
+    # if request.method == "POST":
+        # print(request)
+        # image = request.files["image"]
+        # filename = secure_filename(image.filename)
+        # return jsonify({"message":"hi"})
+        # print(filename)
+        # image.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        # return jsonify({"message": "Image Uploaded Successfully"})
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = '0.0.0.0', port = port, debug=True)
 
 
 # @app.route('/predict', methods=['POST'])
